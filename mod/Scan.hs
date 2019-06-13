a7b378e0fccc3ee49695e0babe0695c22af63864
Dawson Whitehead <dwahme1@gmail.com>
2019-06-13 13:03:26 -0500

    Switched scan to monadic parser

--- mod/Scan.hs
+++ mod/Scan.hs
@@ -1,17 +1,36 @@
 module Scan 
 (
-    Tok,
     scan
 )
 where
 
-data Tok
-    = LParen
-    | RParen
-    deriving (Show)
+import Text.Parsec
+import Text.Parsec.String (Parser)
+import Text.Parsec.Error (messageString)
+import AST
 
-scan :: String -> [Tok]
-scan [] = []
-scan ('(':s) = LParen : (scan s)
-scan (')':s) = RParen : (scan s)
-scan (c:_) = error $ "Illegal token" ++ [c] ++ " in program"
+getUnit :: Parser AST
+getUnit = do
+    try spaces
+    string "()"
+    return Unit
+
+inParens :: Parser AST
+inParens = do
+    try spaces
+    char '('
+    exp <- scanner
+    try spaces
+    char ')'
+    return exp
+
+scanner :: Parser AST
+scanner = do
+    ast <- try getUnit <|> inParens
+    notFollowedBy $ anyToken
+    return ast
+
+scan :: String -> AST
+scan s = case parse scanner "" s of
+    Left err -> error $ show err
+    Right ast -> ast
